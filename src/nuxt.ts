9f0332e48a83d66f364ce91bf758f1d597839cc1
DanCodes <dan@dancodes.online>
2022-10-14 23:26:13 +0100

    feat: add nuxt/kit

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,22 +1,31 @@
+import { defineNuxtModule, isNuxt3 } from '@nuxt/kit'
+import { name } from '../package.json'
+
 import type { Options } from './types'
 import unplugin from '.'
-import { defineNuxtPlugin } from '#imports'
 
-export default function defineNuxtPlugin(nuxtApp) {
-  if (nuxtApp?._version?.startsWith('3.'))
-    options.compiler = 'vue3'
+export default defineNuxtModule<Options>({
+  meta: {
+    name,
+    configKey: 'unplugin',
+  },
+  defaults: {},
+  setup: (options, nuxtApp) => {
+    if (isNuxt3(nuxtApp))
+      options.compiler = 'vue3'
 
-  // install webpack plugin
-  nuxtApp.hook('webpack:config', (configs: any[]) => {
-    configs.forEach((config) => {
-      config.plugins = config.plugins || []
-      config.plugins.unshift(unplugin.webpack(options))
+    // install webpack plugin
+    nuxtApp.hook('webpack:config', (configs: any[]) => {
+      configs.forEach((config) => {
+        config.plugins = config.plugins || []
+        config.plugins.unshift(unplugin.webpack(options))
+      })
     })
-  })
 
-  // install vite plugin
-  nuxtApp.hook('vite:extend', async (vite: any) => {
-    vite.config.plugins = vite.config.plugins || []
-    vite.config.plugins.push(unplugin.vite(options))
-  })
-}
+    // install vite plugin
+    nuxtApp.hook('vite:extend', async (vite: any) => {
+      vite.config.plugins = vite.config.plugins || []
+      vite.config.plugins.push(unplugin.vite(options))
+    })
+  },
+})
